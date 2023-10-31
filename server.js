5759dc3a60da14c3b608228a0d8c7b2e939e943e
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-10-31 13:37:19 +0100

    express server started

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.get("/about", (req, res) => {
   res.send("Esta es la pagina de about")
 })
 
+app.get("/friends/all", (req, res) => {
+  res.send("Esta es la informacion de todos mis amigos")
+})
+
+app.get("/friends/single/:friendName", (req, res) => {
+  // una ruta dinamica => una ruta que tendra un acceso que puede contener cualquier valor posible.
+  console.log(req.params) // ubicacion donde se encuentran todos los parametros dinamicos de esta ruta
+  
+  res.send(`Aqui informacion sobre ${req.params.friendName}`)
+})
+
 const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
